0fb1dfe7549a2c36b28fc17c331d80e8f1fc3393
Luis henrique <105991145+LuissHL@users.noreply.github.com>
2025-05-18 16:49:30 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,7 +1,7 @@
 package com.educandoweb.course.config;
 
 import com.educandoweb.course.entities.User;
-import com.educandoweb.course.repositories.UserRepositoy;
+import com.educandoweb.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +13,7 @@ import java.util.Arrays;
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
     @Autowired
-    private UserRepositoy userRepositoy;
+    private UserRepository userRepositoy;
 
     @Override
     public void run(String... args) throws Exception {
